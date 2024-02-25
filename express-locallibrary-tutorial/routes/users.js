73953ccf8b559b1ed27076c912cba941561395b8
jaomock <obrien1993@hotmail.com>
2024-02-24 16:15:18 -0800

    uploaded

---
+++ express-locallibrary-tutorial/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log('fuck nigga')
+  res.send('respond with a resource');
+});
+
+router.get('/cool', function(req,res,next){
+  console.log('test')
+  res.send('Your\'e ssso cool')
+});
+
+module.exports = router;
